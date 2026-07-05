ba1b69f1efb046f7729ff6b932f753b71abb5371
SEDHU-08 <sedhu5523613@gmail.com>
2026-07-06 01:57:39 +0530

    Configure Django for Railway deployment last

--- simulation_chatbot/settings.py
+++ simulation_chatbot/settings.py
@@ -125,9 +125,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
-)
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = '/media/'
 
