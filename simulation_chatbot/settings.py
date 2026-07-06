306e9520da5c76fca534d9c69cfa931139980a8b
SEDHU-08 <sedhu5523613@gmail.com>
2026-07-06 11:40:57 +0530

    Configure Django for Railway deployment last times

--- simulation_chatbot/settings.py
+++ simulation_chatbot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5_#eq96x)a#k1%5t5oac5gf4e$wu$^sf7bc2@!yh#_j^dny!54'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
